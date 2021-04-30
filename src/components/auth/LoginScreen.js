7d1822561825c278ab69ae8724115034e95f1a1e
alejoboga <alejoboga20@gmail.com>
2021-04-30 14:49:24 -0300

    Adding redux-thunk as middleware

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { login } from '../../actions/auth';
+import { startLoginEmailPassword } from '../../actions/auth';
 import useForm from '../../hooks/useForm';
 
 export const LoginScreen = () => {
@@ -16,7 +16,7 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    dispatch(login(12345, 'Alejo'));
+    dispatch(startLoginEmailPassword(email, passwod));
   };
 
   return (
