b1c26cf7e671140332a45f936db4ad8b6033e3df
alejoboga <alejoboga20@gmail.com>
2021-04-30 15:05:17 -0300

    Adding google sign in

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { startLoginEmailPassword } from '../../actions/auth';
+import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
 import useForm from '../../hooks/useForm';
 
 export const LoginScreen = () => {
@@ -18,6 +18,8 @@ export const LoginScreen = () => {
     e.preventDefault();
     dispatch(startLoginEmailPassword(email, passwod));
   };
+
+  const handleGoogleLogin = () => dispatch(startGoogleLogin());
 
   return (
     <>
@@ -48,7 +50,7 @@ export const LoginScreen = () => {
 
         <div className='auth__social-networks'>
           <p>Login with social networks</p>
-          <div className='google-btn'>
+          <div className='google-btn' onClick={handleGoogleLogin}>
             <div className='google-icon-wrapper'>
               <img
                 className='google-icon'
