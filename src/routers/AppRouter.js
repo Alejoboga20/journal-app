d02303ac6085894a3352d3315bf9969d225ca22b
alejoboga <alejoboga20@gmail.com>
2021-05-03 14:06:32 -0300

    Adding a global Loading state

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { firebase } from '../firebase/firebase-config';
 import {
   BrowserRouter as Router,
@@ -13,14 +13,25 @@ import { login } from '../actions/auth';
 
 export const AppRouter = () => {
   const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
       }
+
+      setChecking(false);
     });
-  }, []);
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if (checking) {
+    return <h1>Wait Up...</h1>;
+  }
 
   return (
     <Router>
