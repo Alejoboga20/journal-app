a7449a949490686c0264d45a527813f78d747eca
alejoboga <alejoboga20@gmail.com>
2021-05-03 09:24:12 -0300

    Adding actions in auth

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,18 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from '../types/types';
+import { finishLoading, startLoading } from './ui';
 
 export const startLoginEmailPassword = (email, password) => {
   return (dispatch) => {
+    dispatch(startLoading());
+
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(({ user }) => {
         const { uid, displayName } = user;
         dispatch(login(uid, displayName));
+        dispatch(finishLoading());
       })
       .catch((e) => console.log(e));
   };
