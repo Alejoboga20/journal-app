2fc34a62c1398e93046cc3ff474d64a90977d25a
alejoboga <alejoboga20@gmail.com>
2021-05-03 09:22:58 -0300

    Adding types for loading state

--- src/types/types.js
+++ src/types/types.js
@@ -3,5 +3,7 @@ export const types = {
   logout: '[auth] Logout',
 
   uiSetError: '[UI] Set Error',
-  uiRemoveError: '[UI] Remove Error'
+  uiRemoveError: '[UI] Remove Error',
+  uiStartLoading: '[UI] Start Loading',
+  uiFinishLoading: '[UI] Finish Loading'
 };
