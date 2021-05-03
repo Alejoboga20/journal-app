4066f8dde0faecd1bf2aa8413529c47b3868b386
alejoboga <alejoboga20@gmail.com>
2021-05-03 14:47:57 -0300

    Adding sweet alerts

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from '../types/types';
 import { finishLoading, startLoading } from './ui';
@@ -17,6 +18,7 @@ export const startLoginEmailPassword = (email, password) => {
       .catch((e) => {
         console.log(e);
         dispatch(finishLoading());
+        Swal.fire('Error', e.message, 'error');
       });
   };
 };
@@ -32,7 +34,10 @@ export const startRegisterWithEmailPasswordName = (email, password, name) => {
         const { uid, displayName } = user;
         dispatch(login(uid, displayName));
       })
-      .catch((e) => console.log(e));
+      .catch((e) => {
+        console.log(e);
+        Swal.fire('Error', e.message, 'error');
+      });
   };
 };
 
