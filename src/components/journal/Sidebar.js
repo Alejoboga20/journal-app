d56b0ee08284c8fcb421801c186d73bcd8b7b627
alejoboga <alejoboga20@gmail.com>
2021-05-04 09:13:41 -0300

    Displaying user name in sidebar

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
 import { JournalEntries } from './JournalEntries';
 
 export const Sidebar = () => {
   const dispatch = useDispatch();
+  const { name } = useSelector((state) => state.auth);
 
   const handleLogout = () => {
     console.log('Here');
@@ -16,7 +17,7 @@ export const Sidebar = () => {
       <div className='journal_sidebar-navbar'>
         <h3 className='mt-5'>
           <i className='far fa-moon'></i>
-          <span> Alejo</span>
+          <span> {name}</span>
         </h3>
 
         <button className='btn' onClick={handleLogout}>
