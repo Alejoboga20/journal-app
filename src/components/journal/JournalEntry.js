ed9d33325431a47fd0931d7e46585b96695b74e6
alejoboga <alejoboga20@gmail.com>
2021-05-06 09:37:42 -0300

    Show notes in sidebar

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,28 +1,29 @@
+import moment from 'moment';
 import React from 'react';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+  const noteDate = moment(date);
+
   return (
     <div className='journal__entry'>
-      <div
-        className='journal__entry-picture'
-        style={{
-          backgroundSize: 'cover',
-          backgroundImage:
-            'url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1200px-Apple_logo_black.svg.png)'
-        }}
-      ></div>
+      {url && (
+        <div
+          className='journal__entry-picture'
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(${url})`
+          }}
+        ></div>
+      )}
 
       <div className='journal__entry-body'>
-        <p className='journal__entry-title'>A new Day</p>
-        <p className='journal__entry-content'>
-          Anim incididunt voluptate ut anim voluptate tempor deserunt duis
-          aliqua sunt fugiat.
-        </p>
+        <p className='journal__entry-title'>{title}</p>
+        <p className='journal__entry-content'>{body}</p>
       </div>
 
       <div className='journal__entry-date-box'>
-        <span>Monday</span>
-        <h4>28</h4>
+        <span>{noteDate.format('dddd')}</span>
+        <h4>{noteDate.format('Do')}</h4>
       </div>
     </div>
   );
