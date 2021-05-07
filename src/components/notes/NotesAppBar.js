2aa10030f448e451102a549a9fc50610bc19b156
alejoboga <alejoboga20@gmail.com>
2021-05-07 11:56:33 -0300

    Updating note in firestore

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { startSaveNote } from '../../actions/notes';
 
 export const NotesAppBar = () => {
+  const { active: note } = useSelector((state) => state.notes);
+  const dispatch = useDispatch();
+
+  const handleSave = () => {
+    console.log('here');
+    dispatch(startSaveNote(note));
+  };
+
   return (
     <div className='notes__appbar'>
       <span>28 de agosto</span>
       <div>
         <button className='btn'>Picture</button>
-        <button className='btn'>Save</button>
+        <button className='btn' onClick={handleSave}>
+          Save
+        </button>
       </div>
     </div>
   );
