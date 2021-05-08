bc3864c55ca6498ad390f534620610eb02d337e5
alejoboga <alejoboga20@gmail.com>
2021-05-08 16:09:56 -0300

    Creating helper to upload file

---
+++ src/helpers/fileUpload.js
@@ -0,0 +1,23 @@
+export const fileUpload = async (file) => {
+  const cloudUrl = 'https://api.cloudinary.com/v1_1/finastra/upload';
+
+  const formData = new FormData();
+  formData.append('upload_preset', 'react-journal-app');
+  formData.append('file', file);
+
+  try {
+    const response = await fetch(cloudUrl, {
+      method: 'POST',
+      body: formData
+    });
+
+    if (response.ok) {
+      const cloudResponse = await response.json();
+      return cloudResponse.secure_url;
+    } else {
+      throw await response.json();
+    }
+  } catch (e) {
+    throw e;
+  }
+};
