ede3cbbff58f343c275b4fce9a765c97bb937a68
alejoboga <alejoboga20@gmail.com>
2021-05-08 16:10:23 -0300

    Triggering action from noteAppBar

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { startSaveNote } from '../../actions/notes';
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 export const NotesAppBar = () => {
   const { active: note } = useSelector((state) => state.notes);
   const dispatch = useDispatch();
 
   const handleSave = () => {
-    console.log('here');
     dispatch(startSaveNote(note));
+  };
+
+  const handlePictureClick = () => {
+    document.querySelector('#fileSelector').click();
+  };
+
+  const handleFileChange = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+
+    if (file) {
+      dispatch(startUploading(file));
+    }
   };
 
   return (
     <div className='notes__appbar'>
       <span>28 de agosto</span>
+
+      <input
+        id='fileSelector'
+        name='file'
+        type='file'
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
       <div>
-        <button className='btn'>Picture</button>
+        <button className='btn' onClick={handlePictureClick}>
+          Picture
+        </button>
         <button className='btn' onClick={handleSave}>
           Save
         </button>
