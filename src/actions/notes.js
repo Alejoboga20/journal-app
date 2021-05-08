33a18fe0601c9f9ab237e73aa19f6b284cd5aeed
alejoboga <alejoboga20@gmail.com>
2021-05-08 16:10:08 -0300

    Adding new action for upload

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,5 +1,6 @@
 import Swal from 'sweetalert2';
 import { db, firebase } from '../firebase/firebase-config';
+import { fileUpload } from '../helpers/fileUpload';
 import { loadNotes } from '../helpers/loadNotes';
 import { types } from '../types/types';
 
@@ -70,3 +71,11 @@ export const refreshNotes = (id, note) => ({
     }
   }
 });
+
+export const startUploading = (file) => {
+  return async (dispatch, getState) => {
+    const { active: activeNote } = getState().notes;
+    const fileUrl = await fileUpload(file);
+    console.log(fileUrl);
+  };
+};
